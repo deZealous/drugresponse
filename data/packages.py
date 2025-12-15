c958c336335a7accb86aaeb89913e29a942e3112
hrm01 <hrm012@gmail.com>
2025-12-16 00:30:29 +0530

    Remove data/packages.py

--- data/packages.py
+++
@@ -1,5 +0,0 @@
-pip install numpy pandas scikit-learn matplotlib seaborn jupyter
-pip install rdkit-pypi deepchem
-pip install torch torchvision torchaudio
-pip install torch-geometric
-pip install transformers
